0eed996d6e3566db7f78df3f48fdebdcdf575d37
EyobAschenaki <ethioeyoba@gmail.com>
2021-03-17 05:39:31 +0300

    didn't do much just wanted to update my clone

--- src/main/java/oms/fx/EducationalRecordsController.java
+++ src/main/java/oms/fx/EducationalRecordsController.java
@@ -1,22 +1,38 @@
 package oms.fx;
 
 import javafx.fxml.Initializable;
+import javafx.scene.chart.ScatterChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EducationalRecordsController implements Initializable {
-    public Label tstLbl;
     public AnchorPane educationalRecords;
-    public ImageView imageV;
     public ScrollPane scrollPane;
+    public ImageView imageView_1;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        File file = new File("place_holder_db.jpg");
+        InputStream stream = null;
+        try {
+            stream = new FileInputStream(file.getAbsoluteFile());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image = new Image(stream);
+        imageView_1.setImage(image);
+
 
     }
+
 }
